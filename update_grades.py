264aa08c20b4d375bd0cdddb2d8d70191ee6acbe
Andy Sayler <andy.sayler@gmail.com>
2013-09-26 17:53:05 -0600

    Set up soem test examples

--- update_grades.py
+++ update_grades.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 
-print("Test")
+import csv
+
+with open('test.csv', newline='') as gradeFile:
+
+    dialect = csv.Sniffer().sniff(gradesheet.read(1024))
+    gradeFile.seek(0)
+    gradereader = csv.reader(gradesheet, dialect)
+
+    for row in gradereader:
+        print(', '.join(row))
