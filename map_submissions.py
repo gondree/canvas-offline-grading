ae6ad9bdc749fa5d883e09f4d260bd3a4828ccd1
Andy Sayler <andy.sayler@gmail.com>
2013-09-27 21:11:04 -0600

    Added new test submissions

--- map_submissions.py
+++ map_submissions.py
@@ -4,7 +4,15 @@
 
 import os
 import subprocess
+import shutil
 
 tempDir = './tmp'
 
 subprocess.check_call(['unzip', '-d', tempDir, 'in.zip'])
+
+for rootdir, subdirs, subfiles in os.walk(tempDir):
+    for subfile in subfiles:
+        if (subfile.split('.')[-1] == 'zip'):
+            print(subfile)
+
+shutil.rmtree(tempDir)
