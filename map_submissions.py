ef2b9435d136ac44da6b39928d6b8c60503c6e65
Andy Sayler <andy.sayler@gmail.com>
2013-09-27 21:32:43 -0600

    Updated mapping to walk through zip levels

--- map_submissions.py
+++ map_submissions.py
@@ -10,9 +10,27 @@ tempDir = './tmp'
 
 subprocess.check_call(['unzip', '-d', tempDir, 'in.zip'])
 
-for rootdir, subdirs, subfiles in os.walk(tempDir):
+for rootDir, subdirs, subfiles in os.walk(tempDir):
+
+    origcwd1 = os.getcwd()
+    print("Entering {!s}".format(rootDir))
+    os.chdir(rootDir)
+
     for subfile in subfiles:
         if (subfile.split('.')[-1] == 'zip'):
-            print(subfile)
+            print("Processing {!s}".format(subfile))
+            subprocess.check_call(['unzip', '-d', tempDir, subfile])
+            origcwd2 = os.getcwd()
+            print("Entering {!s}".format(tempDir))
+            os.chdir(tempDir)
+            print("Submission contains: {!s}".format(os.listdir()))
+            print("Returning to {!s}".format(origcwd2))
+            os.chdir(origcwd2)
+            print("Removing {!s}".format(tempDir))
+            shutil.rmtree(tempDir)
 
+    print("Returning to {!s}".format(origcwd1))
+    os.chdir(origcwd1)
+
+print("Removing {!s}".format(tempDir))
 shutil.rmtree(tempDir)
