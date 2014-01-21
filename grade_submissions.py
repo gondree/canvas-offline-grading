8c36b97a23662c4877f3a89012741ab7e3cf302a
Andy Sayler <andy.sayler@gmail.com>
2014-01-20 22:15:04 -0700

    Added Stats and Better Logging

--- grade_submissions.py
+++ grade_submissions.py
@@ -7,11 +7,20 @@ import os
 import shutil
 import subprocess
 import sys
+import math
 
 import grading_worksheet as gw
 
 TEMPDIR = './tmp'
-ERROR_GRADE = '0.00'
+_FRAMEWORK_LOG_PATH = './framework.log'
+_GRADER_LOG_PATH = './grader.log'
+_ERROR_GRADE = '0.00'
+
+_ZERO_CREDIT = 0
+_FULL_CREDIT = 100
+
+_EXIT_ERROR = -1
+_EXIT_SUCCESS = 0
 
 def _main(argv=None):
     """
@@ -42,18 +51,36 @@ def _main(argv=None):
     # Read Input
     grades, dialect, fields = gw.read_worksheet(input_worksheet)
 
-    subprocess.check_call(['unzip', '-d', TEMPDIR, submissions])
+    # Setup Logs
+    framelog = open(_FRAMEWORK_LOG_PATH, mode='w+t')
+    gradelog = open(_GRADER_LOG_PATH, mode='w+t')
+
+    # Setup Stats
+    found = 0
+    graded = 0
+    scored = 0
+    recorded = 0
+    zeros = 0
+    fulls = 0
+    nerrors = 0
+    ferrors = 0
+    gerrors = 0
+    sumtotal = 0
 
     try:
+        subprocess.check_call(['unzip', '-d', TEMPDIR, submissions], stdout=framelog)
+    except subprocess.CalledProcessError as err:
+        print("ERROR: Could not extract {!s}: {!s}".format(submissions, err), file=sys.stderr)
+        return _EXIT_ERROR;
+    try:
         for root, subdirs, subfiles in os.walk(TEMPDIR):
-
             origwd = os.getcwd()
             script_path = origwd + "/" + grading_script
             print("Entering {!s}".format(root))
             os.chdir(root)
-
             try:
                 for subfile in subfiles:
+                    found += 1
                     subfile_split_dot = subfile.split('.')
                     subfile_split_under = subfile.split('_')
                     if (subfile_split_dot[-1] == 'zip'):
@@ -61,27 +88,41 @@ def _main(argv=None):
                         student = subfile_split_under[0]
                         print("Submission by {!s}".format(student))
                         procdir = ''.join(subfile_split_dot[:-1])
-                        subprocess.check_call(['unzip', '-d', procdir, subfile])
+                        subprocess.check_call(['unzip', '-d', procdir, subfile], stdout=framelog)
                         cwd = os.getcwd()
                         print("Entering {!s}".format(procdir))
                         os.chdir(procdir)
                         print("Running {!s}".format(script_path))
+                        graded += 1
+                        grade = float('nan')
                         try:
-                            output = subprocess.check_output([script_path])
+                            output = subprocess.check_output([script_path], stderr=gradelog)
                             output_str = output.decode()
                             output_str_clean = output_str.rstrip().lstrip()
-                            print("Grade = {!s}".format(output_str_clean))
                             try:
-                                grades[student]['Grade'] = output_str_clean
-                            except KeyError as err:
-                                print("ERROR: Student {!s} not found in grading worksheet: {!s}".format(student, err), file=sys.stderr)
+                                grade = float(output_str_clean)
+                            except:
+                                print("ERROR: Could not convert '{!s}' to float: {!s}".format(output_str_clean, err), file=sys.stderr)
+                                raise
                         except subprocess.CalledProcessError as err:
-                            print("ERROR: Error grading submission: {!s}".format(err), file=sys.stderr)
-                            try:
-                                grades[student]['Grade'] = ERROR_GRADE
-                            except KeyError as err:
-                                print("Student {!s} not found in grading worksheet: {!s}".format(student, err), file=sys.stderr)
+                            print("ERROR: Grading script returned error, check the {:s} log".format(_GRADER_LOG_PATH), file=sys.stderr)
+                            gerrors += 1
                         finally:
+                            if math.isfinite(grade):
+                                scored += 1
+                                if(math.ceil(grade) == _FULL_CREDIT):
+                                    fulls += 1
+                                if(math.floor(grade) == _ZERO_CREDIT):
+                                    zeros += 1
+                                try:
+                                    grades[student]['Grade'] = "{:.2f}".format(grade)
+                                except KeyError as err:
+                                    print("ERROR: Student {!s} not found in grading worksheet: {!s}".format(student, err), file=sys.stderr)
+                                    ferrors += 1
+                                else:
+                                    print("Grade = {}".format(grades[student]['Grade']))
+                                    recorded += 1
+                                    nerrors += 1
                             print("Returning to {!s}".format(cwd))
                             os.chdir(cwd)
                             print("Removing {!s}".format(procdir))
@@ -100,6 +141,22 @@ def _main(argv=None):
     # Write Output
     gw.write_worksheet(output_worksheet, grades, dialect, fields)
 
+    # Close Logs
+    framelog.close()
+    gradelog.close()
+
+    # Print Stats
+    print("Submissions Found:    {}".format(found))
+    print("Submissions Graded:   {}".format(graded))
+    print("Submissions Scored:   {}".format(scored))
+    print("Submissions Recorded: {}".format(recorded))
+    print("Submissions Receiving Full Credit ({:6.2f}): {}".format(_FULL_CREDIT, fulls))
+    print("Submissions Receiving Zero Credit ({:6.2f}): {}".format(_ZERO_CREDIT, zeros))
+    print("Submissions Causing No Errors:        {}".format(nerrors))
+    print("Submissions Causing Framework Errors: {}".format(ferrors))
+    print("Submissions Causing Grader Errors:    {}".format(gerrors))
+
+    return _EXIT_SUCCESS;
 
 if __name__ == "__main__":
     sys.exit(_main())
